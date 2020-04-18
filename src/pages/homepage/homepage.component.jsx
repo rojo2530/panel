e3b07796b2e1fe4623b469da91994e718044bbeb
rojo2530 <jcantosg@gmail.com>
2020-04-19 01:41:31 +0200

    Add homepage component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Layout, Row, Col } from 'antd';
+import './homepage.style.css';
+
+import HeaderPanel from '../../components/header/header.component'
+import SearchBox from '../../components/search-box/search-box.component'
+import ActionsBox from '../../components/actions-box/actions-box.component'
+import OrderBox from '../../components/order-box/order-box.component'
+
+const { Content } = Layout;
+
+const HomePage = () => (
+  <Layout>
+    <HeaderPanel />
+    <Content className="site-layout">
+      <Row className="first-row">
+        <Col span={10}>
+          <SearchBox isPortal={false} />
+        </Col>
+        
+        <Col span={4}>
+          <ActionsBox />
+        </Col>
+        
+        <Col span={10}>
+          <SearchBox isPortal />
+        </Col>
+      </Row>
+
+      <Row className="second-row">
+        <Col span={12}>
+          <OrderBox isPortal={false} />
+        </Col>
+
+        <Col span={12}>
+          <OrderBox isPortal />
+        </Col>
+      </Row>
+
+
+    </Content>
+  </Layout>
+)
+
+export default HomePage
